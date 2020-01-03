60aa1d364f66d1b493c568b0c1505a74e3cb5ce9
Saurav Aggarwal <sauravaggarwal98@gmail.com>
2020-01-03 13:22:46 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishServiceService } from './services/dish-service.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
     
   ],
   imports: [
