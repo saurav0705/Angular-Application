4418ea2308140f40ef24f199cc6f1c16ded53169
Saurav Aggarwal <sauravaggarwal98@gmail.com>
2020-01-04 20:13:40 +0530

    router linking

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Promotion } from '../shared/promotion';
+import { PromotionsService } from '../services/promotions.service';
+import { Dish } from '../shared/dish';
+import {DishServiceService} from '../services/dish-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  dish : Dish;
+  promotion : Promotion;
+  constructor(private dishservice: DishServiceService,
+    private promotionservice: PromotionsService ) { }
 
   ngOnInit() {
+    this.dish = this.dishservice.getFeaturedDish();
+    this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
 }
