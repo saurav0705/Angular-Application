a7686899487e2209f9fa18a7bf762b1a03e62933
Saurav Aggarwal <sauravaggarwal98@gmail.com>
2020-01-05 17:49:27 +0530

    router linking sucess

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishServiceService } from '../services/dish-service.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -7,11 +11,19 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()  
-  dish = Dish;
-  constructor() { }
+   
+  dish : Dish;
+  constructor(private dishservice: DishServiceService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+
+  }
+  goBack(): void {
+    this.location.back();
   }
 
 }
